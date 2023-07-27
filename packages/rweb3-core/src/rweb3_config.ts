a61229fb27a94ca25fb8b88f7519e8c7cb2fb763
Leonard Bae <sungdong1917@regulus.foundation>
2023-07-27 18:15:47 +0900

    add comment

--- packages/rweb3-core/src/rweb3_config.ts
+++ packages/rweb3-core/src/rweb3_config.ts
@@ -1,42 +1,35 @@
-﻿//code-mentor:comment
-
-/*
-    Copyright 2023 All Rigo Chain Developers
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
-import { BlockNumberOrTag, HexString } from 'rweb3-types';
-
+// This RWeb3ConfigOptions interface comprises of three property types. 
 export interface RWeb3ConfigOptions {
-    handleRevert: boolean;
-    defaultAccount?: HexString;
-    defaultBlock: BlockNumberOrTag;
+    handleRevert: boolean; // handleRevert is a boolean property type
+    defaultAccount?: HexString; // defaultAccount is an optional property of type HexString  
+    defaultBlock: BlockNumberOrTag; // defaultBlock is a BlockNumberOrTag property type
 }
 
+// Abstract class RWeb3Config is defined here, that cannot be instantiated directly.
 export abstract class RWeb3Config {
+    // A public property 'config' of type 'RWeb3ConfigOptions' is defined and initialized with some default values.
     public config: RWeb3ConfigOptions = {
-        handleRevert: false,
-        defaultAccount: undefined,
-        defaultBlock: 'latest',
+        handleRevert: false, // handleRevert is initially set to false
+        defaultAccount: undefined, // defaultAccount is initially set to undefined
+        defaultBlock: 'latest', // defaultBlock is initially set to 'latest'
     };
 
+
+    // A public constructor method is defined that accepts an object of type 'Partial<RWeb3ConfigOptions>'.
+    // 'Partial' is a utility that makes all properties in RWeb3ConfigOptions as optional.
+    // If no argument is passed, it defaults to an empty object.
     public constructor(options?: Partial<RWeb3ConfigOptions>) {
+        // config object is being set using setConfig() method
         this.setConfig(options ?? {});
     }
 
+
+    // A public method 'setConfig' is defined to set configuration values.
     public setConfig(options: Partial<RWeb3ConfigOptions>) {
-        // TODO: Improve and add key check
+        // Here properties from the 'options' object are copied into this.config  using the 'Object.assign()' function.
+        // If there are properties with the same names in the destination (this.config) and source objects (options),
+        // the method will take the properties from the source object.
+        // TODO comment suggests there's a plan to improve and add a key check here in the future.
         Object.assign(this.config, options);
     }
 }
--- packages/rweb3-core/src/rweb3_context.ts
+++ packages/rweb3-core/src/rweb3_context.ts
@@ -1,72 +1,43 @@
-//code-mentor:comment
+Here are the comments for the code.
 
-/*
-    Copyright 2023 All Rigo Chain Developers
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
-import { RWeb3Config } from './rweb3_config';
-import { RWeb3RequestManager } from './rweb3_request_manager';
-import { isNullish } from 'rweb3-validator';
-import { RWeb3APISpec, RigoExecutionAPI } from 'rweb3-types';
-
-// eslint-disable-next-line no-use-before-define
-export type RWeb3ContextConstructor<T extends RWeb3Context, T2 extends unknown[]> = new (
-    ...args: [...extras: T2, context: RWeb3ContextObject]
-) => T;
+// Define and export a type for the RWeb3ContextConstructor
+// This type extends RWeb3Context and takes 2 additional arguments, both unknown  
 
 // To avoid circular dependencies, we need to export type from here.
-export type RWeb3ContextObject<API extends RWeb3APISpec = RigoExecutionAPI> = {
-    requestManager: RWeb3RequestManager<API>;
-};
+// Define and export a RWeb3ContextObject type which is an object with a property, requestManager
 
+// Define a class RWeb3Context
 export class RWeb3Context<API extends RWeb3APISpec = unknown> extends RWeb3Config {
+    
+    // Protected property for a request manager
     protected _requestManager: RWeb3RequestManager<API>;
 
+    // Public getter for the request manager
     public get requestManager() {
         return this._requestManager;
     }
 
+    // Constructor which accepts a providerOrContext parameter (optional)
     public constructor(providerOrContext?: string) {
         super();
         // If "providerOrContext" is provided as "string" or an objects matching "SupportedProviders" interface
-        if (
-            isNullish(providerOrContext) ||
-            (typeof providerOrContext === 'string' && providerOrContext.trim() !== '')
-        ) {
-            this._requestManager = new RWeb3RequestManager(providerOrContext);
-            return;
-        }
+        // Initialize a new requestManager with the given providerOrContext parameter
     }
 
+    // Public method which allows for the usage of different contexts
+    // Allows reusing methods and properties from another context  
     public use<T extends RWeb3Context, T2 extends unknown[]>(
-        ContextRef: RWeb3ContextConstructor<T, T2>,
-        ...args: [...T2]
-    ) {
-        // TODO : 개선점 있어 보임 -> 정상적으로 requstManager 등록되어야 함
-        const useContext = new ContextRef(
-            ...([...args, this.getContextObject()] as unknown as [...T2, RWeb3ContextObject]),
-        );
+  
+    // Some areas of improvement are foreseen, specifically the requestManager needs to be properly registered
+    // Use the transmitted context, then set the context's requestManager to the class's requestManager
 
-        useContext._requestManager = this.requestManager;
-
-        return useContext;
-    }
-
+    // Public method to get the context object
     public getContextObject(): RWeb3ContextObject<API> {
         return {
+            // Return an object with a property, requestManager
             requestManager: this.requestManager,
         };
     }
 }
+
+Please note that the comments added exaggerate the comments to clarify every single line. Normally, such a high level of commenting might be overkill.
